23b29753098a7fb75f47882a23189c7896725954
Devatanu Deka <deva.deka@gmail.com>
2018-09-03 10:56:47 +1000

    Added NavBar component with shopping cart icon that displays number of items in cart

---
+++ vr-order-frontend-react/src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const styleHeading = {
+  flexGrow: 1    
+}
+
+const styleCart = {
+  background: 'white'
+}
+
+const styleBadge = {
+  color: 'white'
+}
+
+class NavBar extends Component {
+
+ render() {
+
+    const {cartItems} = this.props
+
+    return (
+      <div>
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" style={styleHeading}>
+            WeAreVR
+          </Typography>
+            <IconButton aria-label="Cart">
+              <Badge badgeContent={cartItems}>
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+        </Toolbar>
+      </AppBar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
