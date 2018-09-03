4c1d41ff658007bbeef1b54b8503233088806c76
Devatanu Deka <deva.deka@gmail.com>
2018-09-03 12:21:13 +1000

    get api/v1/items test initial setup

--- vr-order-backend-rails/spec/controllers/items_controller_spec.rb
+++ vr-order-backend-rails/spec/controllers/items_controller_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
-  it 'test' do
-    
+  describe 'GET /api/v1/items' do
+
+    before do
+      Item.destroy_all
+      Item.create!([
+        {name:"WAVR-XS Glasses", description:"This is the extra small version of the glasses"},
+        {name:"WAVR-SM Glasses", description:"This is the small version of the glasses"},
+        {name:"WAVR-MD Glasses", description:"This is the medium  version of the glasses"},
+        {name:"WAVR-LG Glasses", description:"This is the large version of the glasses"},
+        {name:"WAVR-XL Glasses", description:"This is the extra large version of the glasses"},
+      ])
+    end
+
+    it 'returns all items' do
+      get('/api/v1/items')
+      json = JSON.parse
+    end
+
+    after do
+      Item.destroy_all
+      Item.create!([
+        {name:"WAVR-XS Glasses", description:"This is the extra small version of the glasses"},
+        {name:"WAVR-SM Glasses", description:"This is the small version of the glasses"},
+        {name:"WAVR-MD Glasses", description:"This is the medium  version of the glasses"},
+        {name:"WAVR-LG Glasses", description:"This is the large version of the glasses"},
+        {name:"WAVR-XL Glasses", description:"This is the extra large version of the glasses"},
+      ])
+    end
   end
 end
