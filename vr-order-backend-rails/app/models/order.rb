e269250c8efdb473629eda22146a83b104c4c0d5
Devatanu Deka <deva.deka@gmail.com>
2018-09-04 17:33:00 +1000

    Order model tests passing

--- vr-order-backend-rails/app/models/order.rb
+++ vr-order-backend-rails/app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :shipping_fee, presence: true
+  validates :shipping_fee, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :discount_multiplier, presence: true
+  validates :discount_multiplier, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :discount_multiplier, :numericality => { :less_than_or_equal_to => 1 }
+  validates :total_price, presence: true
+  validates :total_price, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- vr-order-backend-rails/spec/models/order_spec.rb
+++ vr-order-backend-rails/spec/models/order_spec.rb
@@ -1,21 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   it "is valid with valid attributes" do
     order = Order.new(
       name:"P Sherman", 
       address:"42 Wallaby Way, Sydney, Australia", 
       shipping_fee:30,
-      discount_multiplier:1.0)
+      discount_multiplier:1.0,
+      total_price:50)
     expect(order).to be_valid
   end
 
-  it "is not valid without a name"
-  it "is not valid without an address"
-  it "is not valid without a shipping fee"
-  it "is not valid with a negative shipping fee"
-  it "is not valid without a discount multiplier"
-  it "is not valid with a negative discount multiplier"
-  it "is not valid with a discount multiplier more than 1"
+  it "is not valid without a name" do
+    order = Order.new(
+      name:nil, 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:1.0,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid without an address" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:nil, 
+      shipping_fee:30,
+      discount_multiplier:1.0,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid without a shipping fee" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:nil,
+      discount_multiplier:1.0,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid with a negative shipping fee" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:-30,
+      discount_multiplier:1.0,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid without a discount multiplier" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:nil,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid with a negative discount multiplier" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:-1.0,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid with a discount multiplier more than 1.0" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:8.5,
+      total_price:50)
+    expect(order).to be_invalid
+  end
+
+  it "is not valid without a total price" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:0.9,
+      total_price:nil)
+    expect(order).to be_invalid
+  end
+  
+  it "is not valid with a negative total price" do
+    order = Order.new(
+      name:"P Sherman", 
+      address:"42 Wallaby Way, Sydney, Australia", 
+      shipping_fee:30,
+      discount_multiplier:0.9,
+      total_price:-50)
+    expect(order).to be_invalid
+  end
 end
