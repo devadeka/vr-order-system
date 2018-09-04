073fbf24a404cde48a902d966cbdb548aa080832
Devatanu Deka <deva.deka@gmail.com>
2018-09-04 16:36:44 +1000

    item model specs

--- vr-order-backend-rails/app/models/item.rb
+++ vr-order-backend-rails/app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- vr-order-backend-rails/spec/models/item_spec.rb
+++ vr-order-backend-rails/spec/models/item_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is valid with valid attributes" do
+    item = Item.new(
+      name:"WAVR-XL Glasses", 
+      description:"This is the extra large version of the glasses", 
+      price:10)
+    expect(item).to be_valid
+  end
+
+  it "is not valid with a negative price" do
+    item = Item.new(
+      name: "WAVR-XL Glasses", 
+      description: "This is the extra large version of the glasses", 
+      price: -10)
+    expect(item).to be_invalid
+  end
+
+  it "is not valid without a name" do
+    item = Item.new(
+      name: nil, 
+      description:"This is the extra large version of the glasses", 
+      price: 10)
+    expect(item).to be_invalid
+  end
+
+  it "is valid without a description" do
+    item = Item.new(
+      name:"WAVR-XL Glasses", 
+      description: nil, 
+      price: 10)
+    expect(item).to be_valid
+  end
+
 end
