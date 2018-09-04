695fa4cfbe70434281de40c95dd88b8a69df2d6b
Devatanu Deka <deva.deka@gmail.com>
2018-09-04 20:22:24 +1000

    POST /api/v1/orders tests pass

--- vr-order-backend-rails/app/controllers/api/v1/orders_controller.rb
+++ vr-order-backend-rails/app/controllers/api/v1/orders_controller.rb
@@ -3,21 +3,25 @@ module Api
     class OrdersController < ApplicationController
 
       def create
-        order = Order.create!(
-                        name: params[:name], 
-                        address: params[:address],
-                        shipping_fee: 30,
-                        discount_multiplier: 1.0,
-                        total_price: 50
-                      )
+        if params[:items]==nil
+          render :json => {:error => "no items associated with order"}.to_json, :status => 404
+        else
+          order = Order.create!(
+                          name: params[:name], 
+                          address: params[:address],
+                          shipping_fee: 30,
+                          discount_multiplier: 1.0,
+                          total_price: 50
+                        )
 
-        params[:items].each do |item|
-          item["quantity"].to_i.times{
-            OrderItem.create!(order: order, item: Item.find(item["id"].to_i))
-          }
+          params[:items].each do |item|
+            item["quantity"].to_i.times{
+              OrderItem.create!(order: order, item: Item.find(item["id"].to_i))
+            }
+          end
+
+          render :json => {id: order.id}
         end
-
-        render :json => {id: order.id}
       end
 
       def show
