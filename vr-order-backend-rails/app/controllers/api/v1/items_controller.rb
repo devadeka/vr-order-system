317ce5f5c184cd8d88916142e306842f9afdbd23
Devatanu Deka <deva.deka@gmail.com>
2018-09-05 20:34:25 +1000

    api controller cleanup

--- vr-order-backend-rails/app/controllers/api/v1/items_controller.rb
+++ vr-order-backend-rails/app/controllers/api/v1/items_controller.rb
@@ -1,21 +1,18 @@
 module Api
   module V1
     class ItemsController < ApplicationController
-
       def index
-        render json: Item.all
+        render(json: Item.all)
       end
 
       def show
         begin
           item = Item.find(params[:id])
-          render json: item
+          render(json: item)
         rescue Exception => error
-          render :json => {:error => error}.to_json, :status => 404
+          render(:json => {:error => error}.to_json, :status => 404)
         end
       end
-
     end        
   end
 end
-
--- vr-order-backend-rails/app/controllers/api/v1/orders_controller.rb
+++ vr-order-backend-rails/app/controllers/api/v1/orders_controller.rb
@@ -3,28 +3,33 @@ module Api
     class OrdersController < ApplicationController
       include OrdersHelper
       def create
-        if params[:items]==nil
-          render :json => {:error => "no items associated with order"}.to_json, :status => 404
-        else
-          order = Order.create!(
-                          name: params[:name], 
-                          address: params[:address],
-                          shipping_fee: 30,
-                          discount_multiplier: 1.0,
-                          total_price: 50
-                        )
-
-          orderItems = getItemsOfOrder(params[:items])
+        if params[:items].nil?
           
+          render({:json => {:error => "no items associated with order"}.to_json}, {:status => 404})
 
-          order.shipping_fee = getOrderShippingPrice(orderItems)
-          order.discount_multiplier = getDiscountMultiplier(orderItems)
-          order.total_price = ( getItemsTotalPrice(orderItems) + order.shipping_fee ) * order.discount_multiplier
-          order.items = orderItems
+        else
+
+          order = Order.create!(
+            name: params[:name],
+            address: params[:address],
+            shipping_fee: 30,
+            discount_multiplier: 1.0,
+            total_price: 50
+          )
+
+          order_items = get_items_of_order(params[:items])
+
+          order.shipping_fee = get_order_shipping_price(order_items)
+          order.discount_multiplier = get_discount_multiplier(order_items)
+          order.total_price = (getItems_total_price(order_items) +
+            order.shipping_fee) * order.discount_multiplier
+
+          order.items = order_items
 
           order.save
 
-          render :json => {id: order.id}
+          render(:json => {id: order.id})
+          
         end
       end
 
@@ -33,14 +38,12 @@ module Api
           order = Order.find(params[:id])
           order_json = JSON.parse(order.to_json)
           p order.items.to_ary
-          order_json[:items] = getItemQuantityJSON(order.items.to_ary)
+          order_json[:items] = get_item_quantity_JSON(order.items.to_ary)
           render json: order_json
         rescue Exception => error
-          render :json => {:error => error}.to_json, :status => 404
-        end  
+          render({:json => {:error => error}.to_json}, {:status => 404})
+        end
       end
-
-    end        
+    end
   end
 end
-
